be55a81176f06c4f2567bfdd39a5cb9fb1996945
Rob Brightwell <Rob.Brightwell@matchesfashion.com>
2017-09-12 22:20:14 +0100

    Add GetMiddleCharacter class and tests

---
+++ src/main/java/com/codewars/kyu7/GetTheMiddleCharacter.java
@@ -0,0 +1,7 @@
+package com.codewars.kyu7;
+
+/**
+ * Created by robb on 12/09/2017.
+ */
+public class GetTheMiddleCharacter {
+}
---
+++ src/test/java/com/codewars/kyu7tests/GetTheMiddleCharacterTest.java
@@ -0,0 +1,7 @@
+package com.codewars.kyu7tests;
+
+/**
+ * Created by robb on 12/09/2017.
+ */
+public class GetTheMiddleCharacterTest {
+}
