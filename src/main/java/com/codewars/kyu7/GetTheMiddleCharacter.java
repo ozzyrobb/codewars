f64be5586acf629683e6a48d83315826d327cefa
Rob Brightwell <Rob.Brightwell@matchesfashion.com>
2017-09-12 22:25:22 +0100

    Solution for getMiddle

--- src/main/java/com/codewars/kyu7/GetTheMiddleCharacter.java
+++ src/main/java/com/codewars/kyu7/GetTheMiddleCharacter.java
@@ -4,4 +4,10 @@ package com.codewars.kyu7;
  * Created by robb on 12/09/2017.
  */
 public class GetTheMiddleCharacter {
+    public static String getMiddle(String word) {
+        while (word.length() > 2) {
+            word = word.substring(1, word.length() - 1);
+        }
+        return word;
+    }
 }
