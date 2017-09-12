6115b83ba787c8b4468b160e2320105404434e73
Rob Brightwell <Rob.Brightwell@matchesfashion.com>
2017-09-12 22:25:13 +0100

    Add tests for getMiddle

--- src/test/java/com/codewars/kyu7tests/GetTheMiddleCharacterTest.java
+++ src/test/java/com/codewars/kyu7tests/GetTheMiddleCharacterTest.java
@@ -1,7 +1,26 @@
 package com.codewars.kyu7tests;
 
+import com.codewars.kyu7.GetTheMiddleCharacter;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by robb on 12/09/2017.
  */
 public class GetTheMiddleCharacterTest {
+
+    @Test
+    public void evenTests() {
+        assertEquals("es", GetTheMiddleCharacter.getMiddle("test"));
+        assertEquals("dd", GetTheMiddleCharacter.getMiddle("middle"));
+        assertEquals("aa", GetTheMiddleCharacter.getMiddle("aa"));
+    }
+
+    @Test
+    public void oddTests() {
+        assertEquals("t", GetTheMiddleCharacter.getMiddle("testing"));
+        assertEquals("A", GetTheMiddleCharacter.getMiddle("A"));
+        assertEquals("B", GetTheMiddleCharacter.getMiddle("B"));
+    }
 }
